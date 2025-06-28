ccf9365bc5ce6e385126961809b061e9b3f32cce
bbaserdem <baserdemb@gmail.com>
2025-06-28 16:33:59 -0500

    Build Issues
    Fixing problems with build issues

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,6 +2,9 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://storage.googleapis.com/download.flutter.io")
+        }
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
@@ -14,6 +17,9 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://storage.googleapis.com/download.flutter.io")
+        }
     }
 }
 
